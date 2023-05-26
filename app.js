28ab849a45de32efca61416d4606164e2fb5bd2a
Daniel K <dsinaga514@mhs.mdp.ac.id>
2023-05-26 11:28:38 +0700

    MonggoDb
    MonggoDB

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require()
+
+
+require('dotenv/config')
+app.use(bodyParser = require('mongoose'))
+const
 
 app.get('/', (req, res) => {
   res.send('Hello World 2!')
 })
 
+
+mongoose.connect(process.env.DB_CONNECTION, { 
+  useNewUrlParser: true,
+  useUnifiedTopology : true
+})
+
+
+let db = mongoose.connection
+
+
+//handle error
+db.on('error', console.error.bind(console, 'Error establishing a database connection?'))
+
+
+// handle success
+db.once('open',() => {
+  console.log('Database is connected')
+
+})    
+
+
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server running on port  ${process.env.PORT}`)
 })
